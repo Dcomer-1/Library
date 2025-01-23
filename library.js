25101cadcb5e1a083382dbc386ae532183dff8d4
Dcoded <dac409@scarletmail.rutgers.edu>
2025-01-23 11:17:13 -0500

    finished bookcard and submission

--- library.js
+++ library.js
@@ -28,6 +28,18 @@ addBookToLibrary(book2);
 let book3 = new Book('the thed', 'random guy', 295, 'yes');
 addBookToLibrary(book3);
 displayLibrary(myLibrary);
+
+
+
+
+
+<div style="border: 2px solid black; display: flex; flex-direction: column;" class="bookCard">
+            <h1 style="margin-bottom: 0;"> Book Title</h2>
+            <h3> book author</h3>
+            <p style="margin-top: 0;"> Num Of Pages: </p>
+            <p> Read Status: </p>
+
+</div>
 */
 
 
@@ -38,20 +50,52 @@ let numOfPages = 0;
 let readStatus = '';
 
 let buttonElement = document.getElementById('submitButton')
-let bookForm = document.getElementsByClassName("Book-form")
-function handleForm(event){ event.preventDefault();}
+let bookCardContainer = document.getElementById("bookContainer")
+
+function addBookCard(title, author, numPages){
+    let bookCard = document.createElement("div");
+    bookCardContainer.appendChild(bookCard);
+    bookCard.classList.add('bookCard');
+
+    let bookTitle = document.createElement('h1');
+    bookTitle.textContent = title;
+    bookCard.appendChild(bookTitle);
+    bookTitle.classList.add('bookCardTitle');
+
+    let Cardauthor = document.createElement('h3');
+    Cardauthor.textContent = author;
+    bookCard.appendChild(Cardauthor);
+    Cardauthor.classList.add('bookCardAuthor');
+
+    let numOfPages = document.createElement('p');
+    numOfPages.textContent = 'pages: ' + numPages;
+    bookCard.appendChild(numOfPages);
+    numOfPages.classList.add('bookCardPages');
+}
 
 buttonElement.addEventListener("click", function(){
     bookTitle = document.getElementById('title').value;
+    if(bookTitle == '' || bookTitle == null){
+        return;
+    }else{
     author = document.getElementById('author').value;
     numOfPages = document.getElementById('NumberOfPages').value;
     readStatus = document.getElementById('ReadStatus').value;
 
     myLibrary[y] = new Book(bookTitle, author, numOfPages, readStatus);
-    alert(myLibrary);
-    
-
-    
-    
+    addBookCard(bookTitle, author, numOfPages);
     y++;
+    } 
 })
+
+
+
+let createNewBook = document.getElementById('createBook');
+
+createNewBook.addEventListener("click", function(){
+    document.querySelector('.book-information-card').classList.add('active');
+});
+
+document.getElementById('close-button').addEventListener('click', function(){
+    document.querySelector('.book-information-card').classList.remove('active');
+});
