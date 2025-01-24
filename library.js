041f42f322204cf40b4508bde01ca62361afe189
Dcoded <dac409@scarletmail.rutgers.edu>
2025-01-24 15:37:54 -0500

    all functionality completed

--- library.js
+++ library.js
@@ -20,29 +20,6 @@ function displayLibrary(myLibrary){
     }
       
 }
-/*
-let book1 = new Book('the end', 'random guy', 295, 'yes');
-addBookToLibrary(book1);
-let book2 = new Book('the nedd', 'random guy', 295, 'no');
-addBookToLibrary(book2);
-let book3 = new Book('the thed', 'random guy', 295, 'yes');
-addBookToLibrary(book3);
-displayLibrary(myLibrary);
-
-
-
-
-
-<div style="border: 2px solid black; display: flex; flex-direction: column;" class="bookCard">
-            <h1 style="margin-bottom: 0;"> Book Title</h2>
-            <h3> book author</h3>
-            <p style="margin-top: 0;"> Num Of Pages: </p>
-            <p> Read Status: </p>
-
-</div>
-*/
-
-
 
 let bookTitle = '';
 let author = '';
@@ -72,33 +49,59 @@ function addBookCard(title, author, numPages){
     bookCard.appendChild(numOfPages);
     numOfPages.classList.add('bookCardPages');
 
-    let switchClass = document.createElement('label');
-    switchClass.classList.add('switch');
-    let checkBox = document.createElement('input type=checkbox')
-    bookCard.appendChild(switchClass);
-    switchClass.appendChild(checkBox);
+    let btnClass = document.createElement('button');
+    btnClass.textContent = readStatus === 'yes' ? ' Book Read': 'Book Not Read';
+    btnClass.classList.add('read-status-btn', readStatus === 'yes' ? 'read':'notread');
+    bookCard.appendChild(btnClass);
 
-    let slider = document.createElement('span');
-    switchClass.appendChild(slider);
-    slider.classList.add('slider-round');
+    let removeBook = document.createElement('button');
+    removeBook.textContent = 'Remove Book';
+    removeBook.classList.add('remove-book-btn');
+    bookCard.appendChild(removeBook);
 
+    
+    
+    btnClass.addEventListener('click', function(){
+        if(btnClass.classList.contains('read')){
+            btnClass.classList.remove('read');
+            btnClass.classList.add('notread');
+            btnClass.textContent = 'Book Not Read';
+        }else{
+            btnClass.classList.remove('notread');
+            btnClass.classList.add('read');
+            btnClass.textContent = 'Book Read';
+        }
+        
+    });
+
+    removeBook.addEventListener('click', function(){
+        bookCard.remove();
+    })
+    
 }
+
+function clearInput(){
+    document.getElementById('author').value = '';
+    document.getElementById('NumberOfPages').value='';
+    document.getElementById('ReadStatus').value ='';
+    document.getElementById('title').value = '';
+}
+
 
 buttonElement.addEventListener("click", function(){
     bookTitle = document.getElementById('title').value;
-    if(bookTitle == '' || bookTitle == null){
-        return;
-    }else{
     author = document.getElementById('author').value;
     numOfPages = document.getElementById('NumberOfPages').value;
     readStatus = document.getElementById('ReadStatus').value;
-
+    if(bookTitle === '' || author === '' || readStatus ===''|| isNaN(numOfPages)|| readStatus.toLowerCase() !== 'yes' && readStatus.toLowerCase() !=='no'){
+        return;
+    }else{
     myLibrary[y] = new Book(bookTitle, author, numOfPages, readStatus);
     addBookCard(bookTitle, author, numOfPages);
+    clearInput();
     y++;
     } 
-})
-
+});
 
 
 let createNewBook = document.getElementById('createBook');
